b56320bb9c2d10551350f6010914eda287708262
zebanavgurukul <zeba18@navgurukul.org>
2023-01-01 22:09:53 +0530

    commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ db.on('error', () => console.log("Error in Connecting to Database"));
 db.once('open', () => console.log("Connected to Database"))
 
 // post book
-app.post("/books", (req, res) => {
+app.post("/book", (req, res) => {
 
     const {
         title,
@@ -72,14 +72,14 @@ app.post("/books", (req, res) => {
 // get all books
 app.get('/Allbooks', (req, res) => {
     db.collection('customers').find({}).toArray((err, result) => {
-        if(err) throw err
+        if (err) throw err
         res.send(result)
     })
 });
 
 // get by id as isbn
 app.get('/book/:id', (req, res) => {
-    db.collection('customers').findOne({isbn: req.params.id}).then((book) => {
+    db.collection('customers').findOne({ isbn: req.params.id }).then((book) => {
         if (!book) {
             return res.status(404).send();
         }
@@ -87,6 +87,90 @@ app.get('/book/:id', (req, res) => {
     }).catch((error) => {
         res.status(500).send(error)
     })
+});
+
+// app.put("/book/:_id", async (req, res) => {
+//     let data = await db.collection('customers').updateOne(
+//         req.params,
+//         {
+//             $set: req.body
+//         }
+//     );
+//     res.send(data);
+// });
+
+// any update on this book title
+// app.put("/book/:id", async (req, res) => {
+// 	try {
+// 		const post = await db.collection('customers').findOne({ title: req.params.id })
+
+// 		if (req.body.title) {
+// 			post.title = req.body.title
+// 		}
+
+// 		if (req.body.isbn) {
+// 			post.isbn = req.body.isbn
+// 		}
+
+//         if (req.body.pageCount) {
+// 			post.pageCount = req.body.pageCount
+// 		}
+
+// 		if (req.body.publishedDate) {
+// 			post.publishedDate = req.body.publishedDate
+// 		}
+
+//         if (req.body.thumbnailUrl) {
+// 			post.thumbnailUrl = req.body.thumbnailUrl
+// 		}
+
+// 		if (req.body.shortDescription) {
+// 			post.shortDescription = req.body.shortDescription
+// 		}
+
+//         if (req.body.longDescription) {
+// 			post.longDescription = req.body.longDescription
+// 		}
+
+// 		if (req.body.status) {
+// 			post.status = req.body.status
+// 		}
+
+//         if (req.body.authors) {
+// 			post.authors = req.body.authors
+// 		}
+
+// 		if (req.body.categories) {
+// 			post.categories = req.body.categories
+// 		}
+
+// 		await post.save()
+// 		res.send(post)
+// 	} catch {
+// 		res.status(404)
+// 		res.send({ error: "Post doesn't exist!" })
+// 	}
+// });
+
+// delete by book title
+app.delete("/delete/:id", (req, res) => {
+    let deleteID = req.params.id;
+    db.collection('customers').findOneAndDelete(({ title: deleteID }), (err, docs) => {
+        if (err) {
+            throw ((err) => res.status(500).json({ error: "Failed to Delete" }));
+        }
+        else {
+
+            if (docs == null) {
+                res.send("Wrong ID")
+            }
+            else {
+                res.send(docs);
+            }
+
+        }
+
+    });
 });
 
 // replace 3000 with (process.env.PORT)
